83e39c31729bbc00b5f8395b19462a442acc9f50
Shamsaddin <49283898+Shamsaddin010@users.noreply.github.com>
2021-05-27 22:52:23 +0400

    Update MidTerm (Lessons 19 and 20).cpp

--- MidTerm (Lessons 19 and 20).cpp
+++ MidTerm (Lessons 19 and 20).cpp
@@ -1,19 +1,19 @@
-Self-Learning:
+// Self-Learning:
 
-1. Stack - Is kind of an Array which also stores variables.But different from an Array you can input a variable into a stack using push(); or to extract you use pop();
-"stack" is the library name.
+// 1. Stack - Is kind of an Array which also stores variables.But different from an Array you can input a variable into a stack using push(); or to extract you use pop();
+// "stack" is the library name.
 
-2. Queue - The difference between stacks and queues is in removing.In a stack we remove the item the most recently added; in a queue, we remove the item the least recently added.
-
-
-
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-Bu dinamik melumat strukturlaridir. Onlar barede info yigin, methodlarina baxin ve aralirandaki esas ferqleri tapin:
-
-- In Stack elements can be only inserted and deleted only from one side of the list, called the top.
+// 2. Queue - The difference between stacks and queues is in removing.In a stack we remove the item the most recently added; in a queue, we remove the item the least recently added.
 
 
-Yigdiginiz butun melumatlari, ve test etdiyiniz butun kodlari ev tapshirigina gonderin:
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Bu dinamik melumat strukturlaridir. Onlar barede info yigin, methodlarina baxin ve aralirandaki esas ferqleri tapin:
+
+// - In Stack elements can be only inserted and deleted only from one side of the list, called the top.
+
+
+// Yigdiginiz butun melumatlari, ve test etdiyiniz butun kodlari ev tapshirigina gonderin:
 
 #include <iostream> 
 #include <stack> 
